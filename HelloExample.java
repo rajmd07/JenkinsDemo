dd295a10a3de045e919dfe9b1d2e9b33f0d2884f
raj mohammed <raj.its07@gmail.com>
2020-12-24 19:06:29 +0530

    Add files via upload

---
+++ HelloExample.java
@@ -0,0 +1,9 @@
+public class HelloExample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Rajmohammed");
+		System.out.println("Hello Nasreen");
+
+	}
+
+}
---
+++ WelcomeExample.java
@@ -0,0 +1,9 @@
+public class WelcomeExample {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Rajmohammed");
+		System.out.println("Welcome Nasreen");
+
+	}
+
+}
