69c9e11afcabb3b047d0577ff6c4e5b799ea5cb9
raj mohammed <raj.its07@gmail.com>
2020-12-25 11:08:56 +0530

    Update WelcomeExample.java

--- WelcomeExample.java
+++ WelcomeExample.java
@@ -3,6 +3,7 @@ public class WelcomeExample {
 	public static void main(String[] args) {
 		System.out.println("Welcome Rajmohammed");
 		System.out.println("Welcome Nasreen");
+		System.out.println("Welcome Shayan");
 
 	}
 
